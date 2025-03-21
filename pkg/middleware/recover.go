8a88723dd0df2f977bacae79050dc9dd5959dfea
Krzysztof <krzysztof@workflou.com>
2025-03-21 16:37:35 +0100

    refactor handlers

---
+++ pkg/middleware/recover.go
@@ -0,0 +1,17 @@
+package middleware
+
+import "net/http"
+
+func Recover() Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				}
+			}()
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,11 +3,48 @@ package server
 import (
 	"net/http"
 	"workflou/pkg/handler"
+	"workflou/pkg/inmem"
+	"workflou/pkg/middleware"
 )
 
 func New(addr string) *http.Server {
+	userStore := inmem.NewUserStore()
+	sessionStore := inmem.NewSessionStore()
+
+	commonMiddleware := middleware.NewStack(
+		middleware.RequestID(),
+		middleware.LogRequest(),
+		middleware.Recover(),
+	)
+
+	authMiddleware := middleware.NewStack(
+		middleware.AssignUserIdFromCookie(sessionStore),
+		middleware.EnsureAuthenticated(),
+	)
+
+	guestMiddleware := middleware.NewStack()
+
+	mux := http.NewServeMux()
+	authMux := http.NewServeMux()
+
+	authHandler := handler.NewAuthHandler(userStore, sessionStore)
+	authMux.HandleFunc("GET /login", authHandler.LoginPage())
+	authMux.HandleFunc("POST /login", authHandler.LoginForm())
+
+	guestMux := http.NewServeMux()
+	guestMux.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world"))
+	})
+
+	mux.Handle("/", authMiddleware(guestMux))
+	mux.Handle("/auth/", http.StripPrefix("/auth", guestMiddleware(authMux)))
+
+	mux.HandleFunc("GET /panic", func(w http.ResponseWriter, r *http.Request) {
+		panic("oh no")
+	})
+
 	return &http.Server{
 		Addr:    addr,
-		Handler: handler.New(),
+		Handler: commonMiddleware(mux),
 	}
 }
