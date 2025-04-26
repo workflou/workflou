6dcc58b2d85c54a89763a141ef4eca82db49145e
Krzysztof <krzysztof@engineer.com>
2025-04-26 18:02:13 +0200

    test default team after registration

--- tests/Feature/TenancyTest.php
+++ tests/Feature/TenancyTest.php
@@ -1,1 +1,18 @@
 <?php
+
+use App\Models\User;
+use Filament\Auth\Pages\Register;
+use Livewire\Livewire;
+
+test('new user has a default team', function () {
+    Livewire::test(Register::class)
+        ->fillForm([
+            'name' => 'John Doe',
+            'email' => 'john@doe.com',
+            'password' => 'password',
+            'passwordConfirmation' => 'password',
+        ])
+        ->call('register')
+        ->assertHasNoErrors();
+    expect(User::first()->currentTeam)->not->toBeNull();
+});
