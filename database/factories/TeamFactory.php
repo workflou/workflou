3a5f416ec184759f8fd3c477dd7bf3ee83a8672b
Krzysztof <krzysztof@engineer.com>
2025-04-26 18:00:35 +0200

    add unit tests

--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -2,22 +2,20 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\TeamType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Team>
- */
 class TeamFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'slug' => fake()->slug(),
+            'type' => TeamType::Personal,
+            'database' => fake()->word(),
+            'user_id' => User::factory(),
         ];
     }
 }
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,1 +1,18 @@
 <?php
+
+use App\Models\Team;
+use App\Models\User;
+
+test('user has a default team', function () {
+    $user = User::factory()->create();
+
+    expect($user->currentTeam)->not->toBeNull();
+});
+
+test('user can switch team', function () {
+    $user = User::factory()->create();
+    $team = Team::factory()->create();
+    $user->switchTeam($team);
+
+    expect($user->fresh()->currentTeam->id)->toBe($team->id);
+});
