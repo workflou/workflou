a78d45e4520fc5ae3ad2b6af91e295b74968f36a
Krzysztof <krzysztof@engineer.com>
2025-04-27 10:08:35 +0200

    test creating teams

--- tests/Feature/TenancyTest.php
+++ tests/Feature/TenancyTest.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Filament\Pages\Tenancy\RegisterTeam;
+use App\Models\Team;
 use App\Models\User;
 use Filament\Auth\Pages\Register;
 use Livewire\Livewire;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseHas;
 
 test('new user has a default team', function () {
     Livewire::test(Register::class)
@@ -15,4 +20,26 @@ test('new user has a default team', function () {
         ->call('register')
         ->assertHasNoErrors();
     expect(User::first()->currentTeam)->not->toBeNull();
+});
+
+test('user can create a new team', function () {
+    actingAs($user = User::factory()->create());
+
+    Livewire::test(RegisterTeam::class)
+        ->assertSuccessful()
+        ->fillForm([
+            'name' => 'John Doe',
+        ])
+        ->call('register')
+        ->assertHasNoErrors();
+
+    expect($user->teams()->count())->toBe(2);
+
+    assertDatabaseHas('teams', [
+        'name' => 'John Doe',
+        'user_id' => $user->id,
+    ]);
+
+
+    expect(Team::find(2)->users()->count())->toBe(1);
 });
